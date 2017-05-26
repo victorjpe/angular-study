f4294a79e0ab54dca9beb14ef11d761f82e580c2
victorjpe <victor.jose@logicline.de>
2017-05-26 14:15:53 +0530

    Add bootstrap4 and header component in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavigationBarComponent
+],
   imports: [
     BrowserModule,
     FormsModule,
