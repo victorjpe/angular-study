9b5f511fb4548f798433ca1edadce4eeeac114f9
victorjpe <victor.jose@logicline.de>
2017-06-08 12:01:33 +0530

    Add signUp service

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { User } from '../classes/user';
 
 @Injectable()
 export class UserService {
 
-constructor() { }
+	user: User;
+
+	constructor() {
+		this.user = new User();
+	}
+
+	setUser(firstName: string, lastName: string, email: string) {
+		this.user.firstName = firstName;
+		this.user.lastName = lastName;
+		this.user.email = email;
+	}
+
+	getUser() {
+		return this.user;
+	}
 
 }
