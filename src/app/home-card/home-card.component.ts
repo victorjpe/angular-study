1dad95fef276c47344297a71dc042cf4230df773
victorjpe <victor.jose@logicline.de>
2017-06-09 14:54:23 +0530

    Adding animation

--- src/app/home-card/home-card.component.ts
+++ src/app/home-card/home-card.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { Link } from '../../classes/link';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-home-card',
   templateUrl: './home-card.component.html',
-  styleUrls: ['./home-card.component.css']
+  styleUrls: ['./home-card.component.css'],
+  animations: [
+    trigger('iconAnimations', [
+      state('small', style({
+        transform: 'scale(1)',
+      })),
+      state('large', style({
+        transform: 'scale(2.5)',
+      })),
+      transition('small <=> large', animate('100ms linear'))
+    ])
+  ]
 })
 export class HomeCardComponent implements OnInit {
 
+  state: string = 'small';
   constructor() {}
 
   ngOnInit() {
+  }
+
+  animate(event) {
+    if(event === 'enter')
+    this.state = 'large';
+
+    if(event === 'leave')
+    this.state = 'small';
   }
 
   @Input() heading: string = '';
